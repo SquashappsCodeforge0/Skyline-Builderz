cb4944729440cf34bfde08197f36421d581f072a
Subaganeshp <subaganesh@squashapps.com>
2026-07-10 17:10:50 +0530

    404 pages

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/Skyline-Builderz">
     <React.StrictMode>
       <App />
     </React.StrictMode>
